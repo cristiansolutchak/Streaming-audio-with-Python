16b6c724197fc19fff2aa054cee37b8f1054689a
Hierax <94187707+HHierax@users.noreply.github.com>
2023-05-22 12:01:54 -0300

    Add files via upload

---
+++ src/client/client.py
@@ -0,0 +1,98 @@
+import json
+import queue
+import socket
+import threading
+import time
+import pyaudio
+
+SERVER_HOST = "ip do servidor aqui"
+SERVER_TCP_ADDRESS = (SERVER_HOST, 9090)
+SERVER_UDP_ADDRESS = (SERVER_HOST, 9191)
+
+client_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_tcp.connect(SERVER_TCP_ADDRESS)
+
+client_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+audio = pyaudio.PyAudio()
+
+def receive_udp(chunk):
+    data = client_udp.recv(chunk)
+    client_udp.sendto(b'', SERVER_UDP_ADDRESS)
+
+    return data
+
+def load_frames(frames_queue):
+    while True:
+        frames = receive_udp(4096)
+
+        if not frames:
+            break
+            
+        frames_queue.put(frames)
+
+try:
+    song_choices_data = client_tcp.recv(2048)
+    song_choices = json.loads(song_choices_data.decode())
+
+    for index, song in enumerate(song_choices):
+        print(f"{index + 1} - {song['name']}")
+
+    print()
+
+    chosen_song = None
+
+    while not chosen_song:
+        chosen_song_input = input("Insira uma música (id): ")
+
+        if not chosen_song_input.isdigit():
+            print("Valor inválido! Insira apenas inteiros.")
+            continue
+
+        chosen_song_id = int(chosen_song_input) - 1
+
+        for song in song_choices:
+            if song["id"] == chosen_song_id:
+                chosen_song = song
+                break
+        
+        if not chosen_song:
+            print("Valor inválido! Escolha apenas os IDs disponíveis.")
+
+    chosen_song_data = json.dumps(chosen_song).encode()
+    client_tcp.sendall(chosen_song_data)
+
+    audio_stream_configuration_data = client_tcp.recv(1024)
+    audio_stream_configuration = json.loads(audio_stream_configuration_data.decode())
+
+    audio_stream = audio.open(
+        format=audio.get_format_from_width(
+            audio_stream_configuration["width"]
+        ),
+        channels=audio_stream_configuration["channel_amount"],
+        rate=audio_stream_configuration["framerate"],
+        output=True,
+        frames_per_buffer=4096
+    )
+
+    client_udp.sendto(b'',  SERVER_UDP_ADDRESS)
+
+    frames_queue = queue.Queue(maxsize=audio_stream_configuration["frames_amount"])
+
+    load_frames_thread = threading.Thread(
+        target=load_frames, 
+        args=(frames_queue,)
+    )
+    load_frames_thread.start()
+
+    print(f"Agora tocando: {chosen_song['name']}")
+
+    time.sleep(1)
+
+    while not frames_queue.empty():
+        frames = frames_queue.get()
+        audio_stream.write(frames)
+finally:
+    client_tcp.close()
+    audio_stream.close()
+    audio.terminate()
---
+++ src/server/server.py
@@ -0,0 +1,118 @@
+import json
+import os
+import socket
+import threading
+import time
+import wave
+
+HOST = socket.gethostbyname(socket.gethostname())
+TCP_ADDRESS = (HOST, 9090)
+UDP_ADDRESS = (HOST, 9191)
+
+SONGS_PATH = "src/server/songs"
+CHUNK = 1024
+
+server_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_tcp.bind(TCP_ADDRESS)
+
+server_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_udp.bind(UDP_ADDRESS)
+
+server_udp.settimeout(5)
+
+clients = []
+
+songs = []
+
+for song_index, song_path in enumerate(os.listdir(SONGS_PATH)):
+    last_dot_index = song_path.rfind(".")
+    song_name = song_path[:last_dot_index]
+    song = {
+        "id": song_index,
+        "name": song_name, 
+        "path": f"{SONGS_PATH}/{song_path}",
+    }
+
+    songs.append(song)
+
+song_choices_data = json.dumps(songs).encode()
+
+def send_tcp(data, client):
+    client.settimeout(2)
+    client.sendall(data)
+    client.settimeout(None)
+
+def send_udp(message, address):
+    server_udp.sendto(message, address)
+    server_udp.recv(1024)
+
+def disconnect_client(client):
+    clients.remove(client)
+    client.close()
+
+def handle_client(client, client_address):
+    print(f"[CONEXÃO] Nova conexão de: {client_address}")
+
+    clients.append(client)
+
+    print(f"[CONEXÃO] Total de conexões: {threading.active_count() - 1}")
+
+    try:
+        send_tcp(song_choices_data, client)
+
+        chosen_song_data = client.recv(CHUNK)
+        chosen_song = json.loads(chosen_song_data.decode())
+        
+        wave_file = wave.open(chosen_song["path"], "rb")
+
+        audio_stream_configuration = {
+            "width": wave_file.getsampwidth(),
+            "channel_amount": wave_file.getnchannels(),
+            "framerate": wave_file.getframerate(),
+            "frames_amount": wave_file.getnframes(),
+        }
+
+        audio_stream_configuration_data = json.dumps(audio_stream_configuration).encode()
+        send_tcp(audio_stream_configuration_data, client)
+        _, client_udp_address = server_udp.recvfrom(CHUNK)
+
+        while True:
+            frames = wave_file.readframes(CHUNK)
+
+            if not frames:
+                break
+
+            send_udp(frames, client_udp_address)
+            time.sleep(0.01)
+    except (BlockingIOError, ConnectionResetError):
+        pass
+    finally:
+        print(f"[CONEXÃO] Cliente em {client_address} desconectou")
+
+        disconnect_client(client)
+
+        if wave_file:
+            wave_file.close()
+
+def start():
+    server_tcp.listen()
+
+    print(f"[INICIANDO] Servidor iniciando em {HOST} ...")
+
+    while True:
+        client, client_address = server_tcp.accept()
+
+        handle_client_thread = threading.Thread(
+            target=handle_client,
+            args=(client, client_address)
+        )
+
+        handle_client_thread.start()
+
+try:
+    start()
+finally:
+    print("[FECHANDO] Fechando servidor ...")
+
+    server_tcp.close()
+    server_udp.close()
